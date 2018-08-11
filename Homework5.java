0dc97dcdcdfecbd05e1be527505c4994ee6f08a0
Chase Smith <mcsmith01@alumni.amherst.edu>
2018-08-11 09:11:51 -0400

    Added starter code

---
+++ Homework5.java
@@ -0,0 +1,36 @@
+public class Homework5 {
+
+	/* Finish the constructor and create any necessary instance
+	 * variables
+	 */
+	public Homework5(String s, int a, int b) {
+
+	}
+
+	/* Return a substring of the stored String, starting at index with
+	 * length characters. If length would run past the end of the String,
+	 * have the substring end at the end of the stored String.
+	 * For example, if the stored String is "Happy" and the call is made
+	 * substring(2, 5), the method should return "ppy"
+	 */
+	public String substring(int index, int length) {
+
+	}
+
+	/* Return true if the stored String contains the provided String,
+	 * return false if it does not. Note that true and false are
+	 * keywords in Java; I have provided a default return value as
+	 * an example
+	 */
+	public boolean contains(String s) {
+		return false;
+	}
+
+	/* Return the larger of the two stored numbers
+	 */
+	public static int max() {
+
+	}
+	
+
+}
